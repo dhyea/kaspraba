c8bc7b112904fedac7e56426e45157cdd5947e05
dhyea <yesiafrilia173@gmail.com>
2026-07-02 15:34:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // ISI DUA VARIABEL DI BAWAH INI dengan data dari project Supabase kamu
 // Caranya: Supabase Dashboard > Project Settings > API
 // =========================================================
-const SUPABASE_URL = "https://lvktdsueglluglzvkxnl.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://lvktdsueglluglzvkxnl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2a3Rkc3VlZ2xsdWdsenZreG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NzE0MzEsImV4cCI6MjA5ODU0NzQzMX0.u_tl6a3KiDNac-IfD5Bg0nveWmYsrKuvUeyEGHo9qhQ";
